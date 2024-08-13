59b4b97bcd5b2015d3fc61b9f955fb571f7891d6
Tousif Tamboli <tousiftamboli3@gmail.com>
2024-08-14 00:24:05 +0530

    app.jsx completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,81 @@
-import React from 'react'
-import './App.css'
+import React, { useState } from "react";
+import "./App.css";
+import { json } from "react-router-dom";
 
 function App() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch("mongodb://localhost:5000" / api / form, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      if (response.ok) {
+        alert("Form Submitted");
+      } else {
+        alert("Failed");
+      }
+    } catch (error) {
+      console.log("Error:", error);
+      alert("Error submitting form");
+    }
+  };
 
   return (
-    <>
-      <h1>Hello</h1>
-    </>
-  )
+    <div className="app">
+      <h1>Contact Us</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="formElements">
+          <label>
+            Name:
+            <input
+              type="text"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+              required
+            />
+          </label>
+          <label>
+            Email:
+            <input
+              type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              required
+            />
+          </label>
+          <label>
+            Message:
+            <textarea
+              name="message"
+              value={formData.message}
+              onChange={handleChange}
+              required
+            />
+          </label>
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 }
 
-export default App
+export default App;
